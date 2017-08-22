01064d5c94b8771d1228fcb417f56e8e20d148cd
Ali Borhani <aliborhani1@gmail.com>
2017-08-22 23:53:28 +0430

    Fix missing semicolon :|
    // TODO write tests & use fucking CI

--- src/Checkers/Sni.php
+++ src/Checkers/Sni.php
@@ -155,7 +155,7 @@ class Sni
     protected function hasSslError($error)
     {
         return (preg_match('/ssl/ism', $error) || preg_match('/enable crypto/ism', $error))
-            && !preg_match('/timed out/ism', $error)
+            && !preg_match('/timed out/ism', $error);
     }
 
 }
